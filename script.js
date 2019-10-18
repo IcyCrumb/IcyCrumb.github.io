e67b542c126d48af6d096e8dca1d689a54324c05
IcyCrumb <ludysh21@gmail.com>
2019-10-18 14:08:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,87 @@
 function GetURL() {
     alert ("Page URL: " + window.location.href)
-    //return window.location.href
 }
 
 
+function setCookie(cookieName, cookieValue, expireTime, path) {
+    var j = new Date;
+    j.setTime(j.getTime());
+    expireTime && (expireTime *= 864E5);    // 864E5 =  1000*60*60*24 то есть 24 часа в миллисекундах
+    j = new Date(j.getTime() + expireTime);
+    document.cookie = cookieName + "=" + cookieValue + (expireTime ? ";expires=" + j.toGMTString() : "") + (path ? ";path=" + path : "")
+}
 
+function writeLogic(cookieName) {
+    var cookieValue = getTrafficSource(cookieName, 'https://icycrumb.github.io');  // Домен
+    setCookie(cookieName, cookieValue, 182, "/")        // 182 (дня) - это полгода
+};
 
-function SetCookie(value) {
+function getParam(s, q) {
+    try {
+        var match = s.match('[?&]' + q + '=([^&]+)');
+        return match ? match[1] : '';
+    } catch(e) {
+        return '';
+    }
+}
 
-    var TrafficSourceValue = ""
+function calculateTrafficSource() {
+    var source='', medium=''
+    var search_engines = [['bing', 'q'], ['google', 'q'], ['yahoo', 'q'], ['baidu', 'q'], ['yandex', 'q'], ['ask', 'q']];	// Список поисковиков
+    ref = ref.substr(ref.indexOf('//')+2);
+    ref_domain = ref;
 
-    var AdvertisingSubstring = "utm_source"
-    var YandexAdvertisingSubstring = "yandex_direct_name"
-    var GoogleAdvertisingSubstring = "gclid"
+    var url_search = document.location.search;
 
-    if ( value.includes(GoogleAdvertisingSubstring) ) {
-        TrafficSourceValue = "google / cpc"
+    if(url_search.indexOf('utm_source') > -1) {
+        source = getParam(url_search, 'utm_source');
+        medium = getParam(url_search, 'utm_medium');
+    }
+    else if (getParam(url_search, 'gclid')) {
+        source = 'google';
+        medium = 'cpc';
+    }
+    else if(ref) {
+
+        medium = 'referral';
+        source = ref_domain;
+        // Для органического поиска
+        for (var i=0; i<search_engines.length; i++) {
+            if(ref_domain.indexOf(search_engines[i][0]) > -1) {
+                medium = 'organic';
+                source = search_engines[i][0];
+                break;
+            }
+        }
     }
 
-    else if ( value.includes(AdvertisingSubstring) && value.includes(YandexAdvertisingSubstring) ) {
-        TrafficSourceValue = "yandex / cpc"
-    }
+    return {
+        'source'  : source,
+        'medium'  : medium
+    };
+}
 
-    else {
-        TrafficSourceValue = "organic or direct"
-    }
+function getTrafficSource(cookieName, hostname) {
 
+        var trafficSources = calculateTrafficSource();
+        var source = trafficSources.source;
+        var medium = trafficSources.medium;
 
-    document.cookie = "TrafficSource=" + TrafficSourceValue + "; path=/"
+        var rightNow = new Date();
+        var value = source + ' / '  + medium
 
-
-    return
+        return value;
 }
 ;
 
 
-//var CurrentUrl = window.location.href       //GetURL()
-//SetCookie(CurrentUrl)
+// Self-invoking function
+(function() {
 
+    if (document.referrer < 0) {
+        break;
+    } else {
+        writeLogic('TrafficSource');
+    }
 
-
-//alert(document.cookie);
-alert(document.referrer);
+})();
